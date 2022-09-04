59493ebd61cc300e2909a58d2b3d8f3290043889
Hoaveth <hoaveth.gaming@gmail.com>
2022-09-04 23:54:06 +0800

    Added places on the map and also added ratings

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
+import Rating from "@material-ui/lab/Rating";
 
 import useStyles from "./styles";
 import { MAP_API_KEY } from "../../utils/constants";
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(min-midwth: 600px)");
 
@@ -23,7 +23,34 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-      ></GoogleMapReact>
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {isMobile ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography className={classes.typography} variant="subtitle2">
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                  alt={place.name}
+                />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
